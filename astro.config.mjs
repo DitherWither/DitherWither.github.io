c516c86663178c2844b7ce0e9eaa1b557ed2d40f
DitherWither <demonslayervardhan@gmail.com>
2023-04-30 16:39:48 +0530

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://DitherWither.github.io/"
+  site: "https://DitherWither.github.io/",
+  integrations: [tailwind()]
 });
