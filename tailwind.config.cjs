43f341e13d53d56cf11199c7fc0ae87b58f16e1b
DitherWither <demonslayervardhan@gmail.com>
2023-04-30 18:39:18 +0530

    Add dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,20 @@ module.exports = {
 			fontFamily: {
 				sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
 			},
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						"--tw-prose-links": theme("colors.fuchsia.600"),
+						"--tw-prose-invert-links": theme("colors.fuchsia.400"),
+
+						"--tw-prose-bullets": theme("colors.cyan.900"),
+						"--tw-prose-invert-bullets": theme("colors.cyan.50"),
+
+						"--tw-prose-body": theme("colors.cyan.900"),
+						"--tw-prose-invert-body": theme("colors.cyan.50"),
+					}
+				}
+			})
 		},
 	},
 	plugins: [
